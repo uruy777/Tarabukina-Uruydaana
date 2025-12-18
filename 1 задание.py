61c2b11dc461d31e15d3c96430f5ee22acf50297
uruy777 <urujdaanat@gmail.com>
2025-12-18 09:07:18 +0900

    Add files via upload

---
+++ 1 задание.py
@@ -0,0 +1,54 @@
+# 1
+# 1 задание
+"""
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+ if x1 < 1 or x1 > 8 or y1 < 1 or y1 > 8 or x2 < 1 or x2 > 8 or y2 < 1 or y2 > 8:
+     print("ошибка!")
+else:
+    dx = x1 - x2
+    if dx < 0:
+        dy = -dy
+
+    dy = y1 - y2
+    if dy < 0:
+        dy = -dy
+
+    if (dx == 2 and dy == 1) or (dx == 1 and dy == 2):
+        print("да")
+    else:
+        print("нет")"""
+
+# 2 задани
+"""
+k = int(input)
+n = int(input)
+
+s = 0
+i = k 
+
+while i <= n:
+    if i % 2 == 0:
+        s = s + i 
+    i = i + 1 
+
+print(s)"""
+
+# 3 задание
+"""num = 0 
+
+while True: 
+    x = int(input("Введите число: "))
+    if x == 0: 
+        break 
+    num += x 
+print(num)"""
+
+# 4 задание
+"""N = int(input("Введите N:"))
+fact = 1 
+for i in range (1, N + 1): 
+    fact *= I 
+print(fact)"""
